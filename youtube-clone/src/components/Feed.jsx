97f2b37eeb1ccbe5e61e308b7ff993094f243046
Suraj <sonisuraj611@gmail.com>
2022-12-13 22:14:31 +0530

    Added a project

---
+++ youtube-clone/src/components/Feed.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { Box, Typography, Stack } from "@mui/material";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+      let videos = data.items.filter((item) => {
+        return !item.id.playlistId;
+      });
+      setVideos(videos);
+    });
+  }, [selectedCategory]);
+
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          setSelectedCategory={setSelectedCategory}
+          selectedCategory={selectedCategory}
+        />
+        <Typography
+          className="copyright"
+          varaint="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022 SONi MEDIa
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
